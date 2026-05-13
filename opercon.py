b6b5869d514365f084cef2221a91bc2107b0a0f4
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-05-13 22:50:20 +0900

    feat: learn conditions and logical operators

--- opercon.py
+++ opercon.py
@@ -34,3 +34,44 @@ print(id(c), id(d), id(e))
 
 print("c is d", c is d)
 print("c is e", c is e)
+
+print("===== Condition =====")
+x = 15
+
+if x > 50:
+    print("Case A")
+elif x > 10:
+    print("Case B")
+else:
+    print("Case C")
+
+print("===== Logic Operators =====")
+
+age = 21
+
+# person = None
+# if age > 16:
+#     person = "adult"
+# else:
+#     person = "minor"
+
+# Ternary
+person = "adult" if age > 18 else "minor"
+print("person:", person)
+
+print("-----")
+is_student = True
+is_admin = False
+is_guest = True
+is_parent = False
+
+if not is_student:
+    print("Wellcome here, do you want student?")
+elif is_admin:
+    print("Please go to this office!")
+
+# is_guest and is_parent: # and bo'lsa pastdagi yozuv chiqadi.
+elif is_guest or is_parent:
+    print("Waiting room is over there!")
+else:
+    print("Other cases")
