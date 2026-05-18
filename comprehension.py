50e5b79f6974b87ef8c1fe2d2c48c2eda2b7a7f1
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-05-19 00:17:09 +0900

    feat: set & dictionary comprehension

--- comprehension.py
+++ comprehension.py
@@ -21,7 +21,7 @@ print(numbers is list_numbers)
 print(id(numbers), id(list_numbers))
 
 print("-----")
-people = [("Robert", 20), ("Stev", 19), ("Joseph", 25)]
+people = [("Robert", 20), ("Stev", 19), ("Tony", 25)]
 # list_people = [person[] for person in people]  # b version
 list_people = [person[1] for person in people]  # b version
 print("list_people:", list_people)
@@ -35,3 +35,16 @@ cars = [
 ]
 list_cars = [car[0] for car in cars if car[1] > 80]  # c version
 print("list_cars:", list_cars)
+
+print("===== Set and dictionary comp. =====")
+numbs = [1, 5, 4, 20, 4, 5, 1, 4]
+set_numbs = {*numbs}
+print("set_numbs:", set_numbs)
+
+dict_people = {person[0]: person[1] for person in people}  # b version
+print("dict_people:", dict_people)
+
+dict_people2 = {person[0]: person[1]
+                for person in people if person[1] > 20}  # c version
+print("dict_people:", dict_people2)
+# (<expression> for item in iterable) generic type
